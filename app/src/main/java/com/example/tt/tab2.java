64810d6dc0439f059a12043b53549a0ef300cd14
boingkiri <djfelrl11@kaist.ac.kr>
2019-12-28 11:29:33 +0900

    gallery : show gallery

--- app/src/main/java/com/example/tt/tab2.java
+++ app/src/main/java/com/example/tt/tab2.java
@@ -1,6 +1,7 @@
 package com.example.tt;
 
 
+import android.app.Application;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -19,7 +21,7 @@ import java.util.ArrayList;
  * A simple {@link Fragment} subclass.
  */
 public class tab2 extends Fragment {
-    private imageAdapter imgAdapter;
+    private ImageAdapter imgAdapter;
     private GridView gridView;
 
     public tab2() {
@@ -31,49 +33,13 @@ public class tab2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_tab2,container,false);
-        gridView = (GridView)v.findViewById(R.id.mygalleryid);
+        View v = inflater.inflate(R.layout.fragment_tab2,container,true);
+        gridView = v.findViewById(R.id.mygalleryid);
 
-        imgAdapter = new imageAdapter();
-        imgAdapter.addItem(new galleryitem());
-        Log.d("MainActivity",Integer.toString(imgAdapter.getCount()));
-        imgAdapter.addItem(new galleryitem());
-        Log.d("MainActivity",Integer.toString(imgAdapter.getCount()));
+        imgAdapter = new ImageAdapter(getContext().getApplicationContext());
         gridView.setAdapter(imgAdapter);
-        Log.d("MainActivity","CCC");
-        //viewPager.setAdapter(pagerAdapter);
 
         return inflater.inflate(R.layout.fragment_tab2, container, false);
     }
-
-    class imageAdapter extends BaseAdapter {
-        //        ArrayList<galleryitem> items = new ArrayList<galleryitem>();
-        ArrayList<galleryitem> items = new ArrayList<>();
-        @Override
-        public int getCount() {
-            return items.size();
-        }
-
-        public void addItem(galleryitem singerItem){
-            items.add(singerItem);
-        }
-
-        @Override
-        public galleryitem getItem(int i) {
-            return items.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            imageviewer singerViewer = new imageviewer(getContext().getApplicationContext());
-            singerViewer.setItem(items.get(i));
-            return singerViewer;
-        }
-    }
-
 }
+
