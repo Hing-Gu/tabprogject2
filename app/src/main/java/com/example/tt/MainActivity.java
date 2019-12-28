0752f3f5cbda3b28b87b6c6d9879a5d17cda7e5c
Hing-Gu <wp05110@naver.com>
2019-12-28 19:28:20 +0900

    tab1 font

--- app/src/main/java/com/example/tt/MainActivity.java
+++ app/src/main/java/com/example/tt/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.tt;
-
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
