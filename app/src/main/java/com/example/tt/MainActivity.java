3e57a27767c1db5521804de66c6568054904ff73
boingkiri <djfelrl11@kaist.ac.kr>
2019-12-31 17:36:01 +0900

    swipe by using Viewpager

--- app/src/main/java/com/example/tt/MainActivity.java
+++ app/src/main/java/com/example/tt/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.tt;
 
-import android.Manifest;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.FrameLayout;
 
@@ -12,19 +8,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
-
 
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
-   // private ViewPager viewPager;
     private TabItem tab1, tab2,  tab3;
-    public PageAdapter pagerAdapter;
+    public SectionPageAdapter pagerAdapter;
     private FrameLayout frame;
+    private ViewPager mViewpager;
+    SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
+
 
     @SuppressWarnings("ClickableViewAccessibility")
 
@@ -33,79 +30,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         tabLayout=(TabLayout)findViewById(R.id.tabs);
         tab1=(TabItem)findViewById(R.id.tab1);
         tab2=(TabItem)findViewById(R.id.tab2);
         tab3=(TabItem)findViewById(R.id.tab3);
-        frame = findViewById(R.id.frame);
+//        frame = findViewById(R.id.frame);
+
         //viewPager = findViewById(R.id.viewpager);
+        mViewpager = (ViewPager) findViewById(R.id.frame);
+        setupViewPager(mViewpager);
 
+        tabLayout.setupWithViewPager(mViewpager);
 
-        pagerAdapter =new PageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-
-
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                Log.d("MainActivity","tabselectlistener");
-                //viewPager.setCurrentItem(tab.getPosition());
-                Fragment fr = null;
-                if(tab.getPosition()==0) {
-                    //pagerAdapter.notifyDataSetChanged();
-                    fr = new tab1();
-                }else if(tab.getPosition()==1) {
-                    //pagerAdapter.notifyDataSetChanged();
-                    fr = new tab2();
-                } else if(tab.getPosition()==2){
-                   // pagerAdapter.notifyDataSetChanged();
-                    fr = new tab3();
-                }
-                FragmentManager fm = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.frame,fr);
-                fragmentTransaction.commit();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
 
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.frame, new tab1());
         fragmentTransaction.commit();
+    }
 
-        frame.setOnTouchListener(new OnSwipeTouchListener(this) {
-            @Override
-            public void onSwipeRight(){
-                Log.d("setOnTouchListener","Swipe");
-                int current_pos = tabLayout.getSelectedTabPosition();
-                if (current_pos >= 0 && current_pos <= 1){
-                    TabLayout.Tab tab = tabLayout.getTabAt(current_pos + 1);
-                    tab.select();
-                }
-            }
-
-            @Override
-            public void onSwipeLeft(){
-                Log.d("setOnTouchListener","Swipe");
-                int current_pos = tabLayout.getSelectedTabPosition();
-                if (current_pos >= 1 && current_pos <= 2){
-                    TabLayout.Tab tab = tabLayout.getTabAt(current_pos -1);
-                    tab.select();
-                }
-            }
-        });
+    public void setupViewPager(ViewPager viewPager) {
+        adapter.addFragment(new tab1(), "추천");
+        adapter.addFragment(new tab2(), "오늘의 추천");
+        adapter.addFragment(new tab3(), "최근 등록");
+        viewPager.setAdapter(adapter);
     }
 }
 
